9e560de37f0432211520adde5708282678c1df8f
Stefan Romberg <romberg@navvis.com>
2017-11-25 16:44:19 +0100

    * More work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import {
+  IKeyboardLayout, IKeyboardLayouts, keyboardLayouts, MAT_KEYBOARD_LAYOUTS, MatKeyboardComponent, MatKeyboardModule, MatKeyboardRef,
+  MatKeyboardService
+} from '@ngx-material-keyboard/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  private _keyboardRef: MatKeyboardRef<MatKeyboardComponent>;
+
+  darkTheme: boolean;
+  duration: number;
+  hasAction: boolean;
+  isDebug: boolean;
+  defaultLocale: string;
+  layout: string;
+
+  layouts: {
+    name: string;
+    layout: IKeyboardLayout;
+  }[];
+
+  constructor(private _keyboardService: MatKeyboardService,
+              @Inject(LOCALE_ID) public locale,
+              @Inject(MAT_KEYBOARD_LAYOUTS) private _layouts) {
+    // console.log(locale);
+    // console.log(_layouts);
+  }
+
+  ngOnInit() {
+    this.defaultLocale = ` ${this.locale}`.slice(1);
+    this.layouts = Object.keys(this._layouts)
+      .map((name: string) => (
+        {
+          name: name,
+          layout: this._layouts[name]
+        }
+      ))
+      .sort((a, b) => a.layout.name.localeCompare(b.layout.name));
+  }
+
+
+  openKeyboard(locale = this.defaultLocale) {
+    this._keyboardRef = this._keyboardService.open(locale, {
+      darkTheme: this.darkTheme,
+      duration: this.duration,
+      isDebug: this.isDebug
+    });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
+import { IKeyboardLayouts, keyboardLayouts, MAT_KEYBOARD_LAYOUTS, MatKeyboardModule } from '@ngx-material-keyboard/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // Material modules
+    MatButtonModule,
+    MatKeyboardModule,
+
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    // { provide: MAT_KEYBOARD_LAYOUTS, useValue: customLayouts }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
